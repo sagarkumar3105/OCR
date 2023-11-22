4be897b93922465fa52da50bea0183b54f00fa99
Sagar Kumar <74974420+sagarkumar3105@users.noreply.github.com>
2023-11-23 01:11:26 +0530

    Update invoApp_tika.py

--- invoApp_tika.py
+++ invoApp_tika.py
@@ -12,6 +12,15 @@ import pandas as pd
 import os
 
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://sagarinvoapp:fT5fr@cluster0.sap1yb0.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db=client["Cluster0"]
+print(db.list_collection_names())
+
 def findAllIdx(exp,tlist):
     pattern=re.compile(exp,re.I)
     try:
@@ -44,6 +53,7 @@ def writeJsonJainChem():
             f.write(jd)
 
 def jainChemTemplate(dl,date,invoNum):
+    print("!!!!!!!!!!!!!!!!!!!")
     vendor='Jain Chemicals'
     head=['Vendor','Date','Invoice Number','Description of Goods', 'HSN/SAC', 'Quantity', 'Rate', 'per', 'Amount']
     dog=[]
@@ -52,7 +62,6 @@ def jainChemTemplate(dl,date,invoNum):
     rate=[]
     per=[]
     amt=[]
-    
     for r in dl[1:]:
         r=r.split(" ")
         print(r,"\n")
@@ -75,23 +84,34 @@ def jainChemTemplate(dl,date,invoNum):
         rate.append(r[-3])
         per.append(r[-2])
         amt.append(r[-1])
-        
-    vendorList=[vendor.upper()]*len(dog)
-    masterData=dict(zip(head,[vendor,date,invoNum,dog,hsn,qty,rate,per,amt]))
-    ledgerData=dict(zip(head[1:],[date,invoNum,dog,hsn,qty,rate,per,amt]))
+    
+    ledgerData={"Date":date,"Invoice Number":invoNum,'Description of Goods':dog, 'HSN/SAC':hsn, 'Quantity':qty, 'Rate':rate, 'per':per, 'Amount':amt}
+    masterData={"Jain Chemicals":ledgerData}
+
+    writeToDb("jainChemicals",ledgerData,masterData)
+    #vendorList=[vendor.upper()]*len(dog)
+    # masterData=dict(zip(head,[vendor,date,invoNum,dog,hsn,qty,rate,per,amt]))
+    # ledgerData=dict(zip(head[1:],[date,invoNum,dog,hsn,qty,rate,per,amt]))
     #data[vendor[0]]=data
     
-    mdf=pd.DataFrame(masterData)
-    mdf.to_csv("MasterData.csv",mode='a',index=False,header=False)
+    # mdf=pd.DataFrame(masterData)
+    # mdf.to_csv("MasterData.csv",mode='a',index=False,header=False)
     
-    ldf=pd.DataFrame(ledgerData)
-    ldf.to_csv("Ledgers/"+vendor.upper()+".csv",mode='a',index=False,header=False)
+    # ldf=pd.DataFrame(ledgerData)
+    # ldf.to_csv("Ledgers/"+vendor.upper()+".csv",mode='a',index=False,header=False)
     #df.to_json("Invo_res.json")
-    
+
+def writeToDb(tableName,tableData,MasterData):
+    global db
+    db[tableName].insert_one(tableData)
+    db["MasterData"].insert_one(MasterData)
+    print("inster Record")
+
+ 
 def kpkTemplate(dataList):
     pronumIdx=findIdx(".*pro no.*",dataList,0)
     invoiceDateIdx=findIdx(".*[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{2,4}.*",dataList,0)
-    ponumIdx=findIdx(".*po.*",dataList,1)
+    ponumIdx=findIdx(".*po.*",dataList,0)
     tableHeaderIdx=findIdx(".*CAT NO PRODUCT NAME.*",dataList,0)
     totalIdx=findIdx(".*Total.*",dataList,)
     gstIdx=findIdx(".*gst.*",dataList)
@@ -105,10 +125,12 @@ def kpkTemplate(dataList):
     net=dataList[netIdx].split()[-1]
 
     dataList=dataList[tableHeaderIdx+1:totalIdx]
-
-    data=[pronum,invoiceDate,ponum,total,gst,net]
-    #print(data)
     data2=[]
+    prodList=[]
+    qtyList=[]
+    amtList=[]
+    rateList=[]
+    unitList=[]
     for d in dataList:
         line=d.split()
         amt=line[-1]
@@ -127,79 +149,31 @@ def kpkTemplate(dataList):
             k=-5
         prod=" ".join(line[0:k])[2:]
         prod=re.sub(r'[^\w\s\(\)]', "", prod)
-        row=["KPK_SCIENTIFIC_SUPPLIES",invoiceDate,pronum,prod,"",unit,rate,'',amt]
-        data2.append(row)
-    return data,data2
+        #row=["KPK_SCIENTIFIC_SUPPLIES",invoiceDate,pronum,prod,"",unit,rate,'',amt]
+        #data2.append(row)
+        prodList.append(prod)
+        qtyList.append(qty)
+        amtList.append(amt)
+        rateList.append(rate)
+        unitList.append(unit)
+
+    ledgerData={"PRO Num":pronum,"Date":invoiceDate,"PO Num":ponum,"Total":total,"GST @18%":gst,"Net Amount":net}
+    masterData={"KPK_SCIENTIFIC_SUPPLIES":{'Date':invoiceDate,'Invoice Number':ponum,'Description of Goods':prodList,'HSN/SAC':["" for _ in range(len(prodList))],'Quantity':qtyList,'Rate':rateList,'per':unitList,'Amount':amtList}}
+
+    writeToDb("KPKScientificSupplies",ledgerData,masterData)
 
 
-# def imProcess(fname):
-    # doc = fitz.open(fname)
-    # for i in range(doc.page_count):
-        # page=doc.load_page(i)
-        # #page.clean_contents()
-        # pageText=page.get_text("text") 
-        
-        # pix=page.get_pixmap(dpi=180)
-        
-        # pimg = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-        # img=np.asarray(pimg)
 
-        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # img = cv2.bitwise_not(gray)
-        # # cv2.imshow("xyz",img)
-        # # cv2.waitKey(0)
-  
-        # # cv2.destroyAllWindows()
-        
-        # text = pytesseract.image_to_string(img,config='-c preserve_interword_spaces=0 --psm 4')
-
-        # dl=text.split("\n")
-        # endPtrn=re.compile("Output CGST.*",re.I)
-
-        # # print("-------should be>",dl.index("Output CGST @9 % 9 % 111.48"))
-        # vendor=''
-        # date=''
-        # invoNum=''  
-        
-        # tableStartPtrn=re.compile(".*Description of Goods.*")
-        # tableStartIdx=[dl.index(i) for i in list(filter(tableStartPtrn.match,dl))][0]
-        
-        
-        # if(i==0):          
-            # vendor=" ".join(dl[2].split(" ")[:-3])
-            # dtPtrn=re.compile(".*[0-9]{1,2}-[A-Za-z]+-[0-9]{2,4}.")
-            # date=list(filter(dtPtrn.match,dl))[0].split(" ")[-1]
-            # invoNum=list(filter(dtPtrn.match,dl))[0].split(" ")[-2]
-            # dl=dl[tableStartIdx:]
-            # try:
-                # endIdx=[dl.index(i) for i in list(filter(endPtrn.match,dl))][0]
-                # dl=dl[:endIdx-1]
-            # except IndexError:
-                # pass
-
-        # if(i>0):
-            # dl=dl[tableStartIdx:]
-            # endIdx=[dl.index(i) for i in list(filter(endPtrn.match,dl))][0]
-            # dl=dl[:endIdx-1]
-            
-            
-        # getTableDate(dl,vendor,date,invoNum)
-        
-        # print(text)
-        # return text
-        
-#txt=imProcess("jainchem.pdf")
-
-
+#------------------------------>client.close()
 
 
 def user():
-    st.title("Invoice Proecessing System")
-    ufiles=st.file_uploader("Upload a pdf file",accept_multiple_files=True)
+    st.title("Invoice Processing System")
+    ufiles=st.file_uploader("Upload a PDF file",accept_multiple_files=True)
     go=st.button("Process")
     kpk=re.compile(".*kpk.*",re.IGNORECASE)  
-
-    if(go):
+    successPrompt="Details Extracted"
+    if(go and ufiles):
         # try:
             for f in ufiles:
                 doc=''
@@ -208,36 +182,20 @@ def user():
                 except:
                     st.error("File Rendering failed Contact Developer")
                 res=doc['content']
+                st.write(res)
                 dataList=res.split("\n")
                 #imProcess(f)#"jainchem.pdf")#r"../Main Demo/invo.png")
                 dataList=[i.strip() for i in dataList if len(i.strip())>0] # Removing all empty lines and removing trailing spaces
                 if len(tuple(filter(kpk.match, dataList))):
                     #data=[pronum,invoiceDate,ponum,total,gst,net]
-                    head1=["PRO Num","Date","PO Num","Total","GST @18%","Net Amount"]
-                    head2=['Vendor','Date','Invoice Number','Description of Goods','HSN/SAC','Quantity','Rate','per','Amount']
-                    data1,data2=kpkTemplate(dataList)
-                        
-                    dfData1=dict(zip(head1,data1)) # Puting all the extracted data into respective columns
-                    kpkDf1=pd.DataFrame(dfData1,index=[0])
-                    kpkDf1.to_csv(r"Ledgers\KPK_SCIENTIFIC_SUPPLIES.csv",mode='a',index=False,header=False) #Writing the data
-                    tempdf=[] #Container to hold each row of invoice
-                    for d in data2:
-                        tempdf.append(dict(zip(head2,d)))
-                    data2=tempdf
-                    kpkDf2=pd.DataFrame(data2,index=range(len(data2)))
-                    kpkDf2.to_csv(r"MasterData.csv",mode='a',index=False,header=False)
+                    kpkTemplate(dataList)
                 else:
+                    
                     st.warning("Got a new invoice Type")
-            st.success("Details Extracted")
-        # except e:
-            # st.error("Check the file Formate. The uploaded file doens't follow the predescribed formate")
-#@st.cache()
-# def getvName(led):
-    # return st.multiselect("Select the vendor Name",led)
-#@st.cache(suppress_st_warning=True)
-# def Authenticate():
-    
-    # return session
+                    successPrompt="!!! Details Not Extracted"
+            st.success(successPrompt)
+
+
 def admin():
     st.title("Admin Portal")
     session=False
@@ -252,30 +210,37 @@ def admin():
             st.error("Permission denied. Try with correct credentials")
     
     if session:
+        global db
         st.markdown("**Session Authenticated**")
-        if not os.path.isfile("/Ledgers/"):
-            os.makedirs("Ledgers", exist_ok=True)
-        led=[i.split(".")[0] for i in os.listdir("Ledgers/")]
+        listOfAllLedgers=db.list_collection_names()
+        print(listOfAllLedgers)
+        if len(listOfAllLedgers)>=0:
+            numOfLedgers=len(listOfAllLedgers)-1
+        else:
+            numOfLedgers=0
+        st.info("Total Number of Available Ledgers/Vendors: "+str(numOfLedgers))
 
-        st.info("Total Number of Available Ledgers/Vendors: "+str(len(led)))
-
-        vName=st.multiselect("Select the vendor Name",led)
+        vName=st.multiselect("Select the vendor Name",listOfAllLedgers)
         #getvName(led)
-        df=pd.read_csv("MasterData.csv")
-        res=pd.DataFrame(columns=df.columns.tolist())
-        #show=st.button('show')
-        #if show:
-        saveFileName=''
-        exportButton=''
-        with st.expander("Export File"):
-            saveFileName=st.text_input("Enter a file name to export the below result as CSV")
-            exportButton=st.button("Export")
-        for i in vName:
-            res=pd.concat([res,df[df['Vendor']==i]])
-        st.dataframe(res)
-        if(exportButton):
-            res.to_csv(saveFileName+".csv")
-            st.balloons()#("File saved in local machine")
+        collection=db["MasterData"]
+        cursor=collection.find()
+        MasterDf=pd.DataFrame(list(cursor))
+        st.table(MasterDf)
+        # df=pd.read_csv("MasterData.csv")
+        # res=pd.DataFrame(columns=df.columns.tolist())
+        # #show=st.button('show')
+        # #if show:
+        # saveFileName=''
+        # exportButton=''
+        # with st.expander("Export File"):
+        #     saveFileName=st.text_input("Enter a file name to export the below result as CSV")
+        #     exportButton=st.button("Export")
+        # for i in vName:
+        #     res=pd.concat([res,df[df['Vendor']==i]])
+        # st.dataframe(res)
+        # if(exportButton):
+        #     res.to_csv(saveFileName+".csv")
+        #     st.balloons()#("File saved in local machine")
         
         
     else:
